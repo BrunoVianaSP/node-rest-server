f77db85381635213ca9af947a7b77897d2927332
Bruno Barbosa <bruno.barbosa@keyrus.com.br>
2018-08-24 19:38:56 -0300

    Working fine

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-// content of index.js
-const http = require('http')
-const port = 3002
+const express = require('express');
+const app = express();
+const router = express.Router();
+const port = 3002;
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+// url: http://localhost:3000/
+app.get('/', (request, response) => response.send('Hello World'));
 
-const server = http.createServer(requestHandler)
+// all routes prefixed with /api
+app.use('/api', router);
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
+// using router.get() to prefix our path
+// url: http://localhost:3000/api/
+router.get('/', (request, response) => {
+  response.json({message: 'Hello, welcome to my server'});
+});
+
+// set the server to listen on port 3000
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+
+const url = require('url');
+
+router.get('/stuff', (request, response) => {
+  var urlParts = url.parse(request.url, true);
+  var parameters = urlParts.query;
+  var myParam = parameters.myParam;
+  // e.g. myVenues = 12;
+  
+  var myResponse = `I multiplied the number you gave me (${myParam}) by 5 and got: ${myParam * 5}`;
+  
+  response.json({message: myResponse});
+});
+
+
+// this array is used for identification of allowed origins in CORS
+const originWhitelist = ['http://localhost:3000', 'https://example.net'];
+
+// middleware route that all requests pass through
+router.use((request, response, next) => {
+  console.log('Server info: Request received');
+  
+  let origin = request.headers.origin;
+  
+  // only allow requests from origins that we trust
+  if (originWhitelist.indexOf(origin) > -1) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
   }
-
-  console.log(`server is listening on ${port}`)
-})
+  
+  // only allow get requests, separate methods by comma e.g. 'GET, POST'
+  response.setHeader('Access-Control-Allow-Methods', 'GET');
+  response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  
+  // push through to the proper route
+  next();
+});
