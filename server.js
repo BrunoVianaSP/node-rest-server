c3f431f0f7182cbe4d23e26beeb0dc362155b427
Bruno Barbosa <bruno.barbosa@keyrus.com.br>
2018-08-30 14:50:41 -0300

    Trying online mongo but not worked yet

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
-const port = 3000;
+const port = 3013;
 const config = require('./db');
 const User = require('./models/User');
 const userRoute = require('./routes/route_user');
@@ -19,16 +19,35 @@ app.get('/', (request, response) => response.send('Hello World'));
 // all routes prefixed with /api
 // app.use('/api', router);
 
-// connect to database
+// connect to database local
 mongoose.connect(config.DB).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database' +err)
-});
+    () => { console.log('Database is connected') },
+    err => {
+        console.log('Can not connect to the database' + err)
+    });
+
+// mongoose.connect('mongodb+srv://kay:f2320419@cluster0.mongodb.net/test').then(
+//     () => { console.log('Database is connected') },
+//     err => {
+//         console.log('Can not connect to the database:\n' + err)
+//     });
+
+
+// // connect to database online
+// var MongoClient = require('mongodb').MongoClient;
+
+// var uri = "mongodb+srv://kay:f2320419@cluster0.mongodb.net/user-management-db";
+// MongoClient.connect(uri, function(err, client) {
+//     const collection = client.db("user-management-db").collection("user");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
 
 // using router.get() to prefix our path
 // url: http://localhost:3000/api/
 router.get('/', (request, response) => {
-  response.json({message: 'Hello, welcome to my server'});
+    response.json({ message: 'Hello, welcome to my server' });
 });
 
 
@@ -67,14 +86,14 @@ router.get('/', (request, response) => {
 const url = require('url');
 
 router.get('/stuff', (request, response) => {
-  var urlParts = url.parse(request.url, true);
-  var parameters = urlParts.query;
-  var myParam = parameters.myParam;
-  // e.g. myVenues = 12;
-  
-  var myResponse = `I multiplied the number you gave me (${myParam}) by 5 and got: ${myParam * 5}`;
-  
-  response.json({message: myResponse});
+    var urlParts = url.parse(request.url, true);
+    var parameters = urlParts.query;
+    var myParam = parameters.myParam;
+    // e.g. myVenues = 12;
+
+    var myResponse = `I multiplied the number you gave me (${myParam}) by 5 and got: ${myParam * 5}`;
+
+    response.json({ message: myResponse });
 });
 
 
@@ -83,22 +102,22 @@ const originWhitelist = ['http://localhost:3000', 'https://heroku-test-node-api.
 
 // middleware route that all requests pass through
 router.use((request, response, next) => {
-  console.log('Server info: Request received');
-  
-  let origin = request.headers.origin;
-  
-  // only allow requests from origins that we trust
-  if (originWhitelist.indexOf(origin) > -1) {
-    response.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  
-  // only allow get requests, separate methods by comma e.g. 'GET, POST'
-  response.setHeader('Access-Control-Allow-Methods', 'GET');
-  response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  response.setHeader('Access-Control-Allow-Credentials', true);
-  
-  // push through to the proper route
-  next();
+    console.log('Server info: Request received');
+
+    let origin = request.headers.origin;
+
+    // only allow requests from origins that we trust
+    if (originWhitelist.indexOf(origin) > -1) {
+        response.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    // only allow get requests, separate methods by comma e.g. 'GET, POST'
+    response.setHeader('Access-Control-Allow-Methods', 'GET');
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    response.setHeader('Access-Control-Allow-Credentials', true);
+
+    // push through to the proper route
+    next();
 });
 
 
