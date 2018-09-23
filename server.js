2c85bb67f9aa4297db46cd54f47b9481f2d9bffc
Bruno Barbosa <bruno.barbosa@keyrus.com.br>
2018-09-23 20:33:04 -0300

    test endpoints

--- server.js
+++ server.js
@@ -32,6 +32,58 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
 
 
 
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({ "error": message });
+}
+
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+app.get("/api/contacts", function(req, res) {
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
+});
+
+app.post("/api/contacts", function(req, res) {
+    var newContact = req.body;
+    newContact.createDate = new Date();
+
+    if (!req.body.name) {
+        handleError(res, "Invalid user input", "Must provide a name.", 400);
+    } else {
+        db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+            if (err) {
+                handleError(res, err.message, "Failed to create new contact.");
+            } else {
+                res.status(201).json(doc.ops[0]);
+            }
+        });
+    }
+});
+
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/api/contacts/:id", function(req, res) {});
+
+app.put("/api/contacts/:id", function(req, res) {});
+
+app.delete("/api/contacts/:id", function(req, res) {});
+
+
+
+
 
 // const express = require('express');
 // const bodyParser = require('body-parser');
