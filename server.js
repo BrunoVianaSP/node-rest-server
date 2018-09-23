6ea71e6dab50ff9090af9f3483d968299f195c65
Bruno Barbosa <bruno.barbosa@keyrus.com.br>
2018-09-23 20:26:51 -0300

    mongo

--- server.js
+++ server.js
@@ -1,27 +1,123 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const app = express();
-const router = express.Router();
 const port = 3000;
-const config = require('./db');
-const User = require('./models/User');
-const userRoute = require('./routes/route_user');
-const url = require('url');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
 app.use(bodyParser.json());
 
-app.use('/user', userRoute);
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
-// url: http://localhost:3000/
-app.get('/', (request, response) => response.send('Hello World'));
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function(err, client) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-app.get('/api', (request, response) => response.send('This the API'));
+    // Save database object from the callback for reuse.
+    db = client.db();
+    console.log("Database connection ready");
+
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || port, function() {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
+});
+
+
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+
+// const app = express();
+// const router = express.Router();
+// const port = 3000;
+// const config = require('./db');
+// const User = require('./models/User');
+// const userRoute = require('./routes/route_user');
+// const url = require('url');
+
+// // this array is used for identification of allowed origins in CORS
+// const originWhitelist = ['http://localhost:3000', 'https://heroku-test-node-api.herokuapp.com/'];
+
+// app.use(bodyParser.json());
+
+// app.use('/user', userRoute);
+
+// // url: http://localhost:3000/
+// app.get('/', (request, response) => response.send('Hello World'));
+
+// app.get('/api', (request, response) => response.send('This the API'));
+
+// router.get('/stuff', (request, response) => {
+//     var urlParts = url.parse(request.url, true);
+//     var parameters = urlParts.query;
+//     var myParam = parameters.myParam;
+//     // e.g. myVenues = 12;
+
+//     var myResponse = `I multiplied the number you gave me (${myParam}) by 5 and got: ${myParam * 5}`;
+
+//     response.json({ message: myResponse });
+// });
+
+
+// // middleware route that all requests pass through
+// router.use((request, response, next) => {
+//     console.log('Server info: Request received');
+
+//     let origin = request.headers.origin;
+
+//     // only allow requests from origins that we trust
+//     if (originWhitelist.indexOf(origin) > -1) {
+//         response.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+
+//     // only allow get requests, separate methods by comma e.g. 'GET, POST'
+//     response.setHeader('Access-Control-Allow-Methods', 'GET');
+//     response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     response.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // push through to the proper route
+//     next();
+// });
+
+// app.listen(process.env.PORT || port, function() {
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // all routes prefixed with /api
 // app.use('/api', router);
-
 // connect to database local
 // mongoose.connect(config.DB).then(
 //     () => { console.log('Database is connected') },
@@ -71,52 +167,3 @@ app.get('/api', (request, response) => response.send('This the API'));
 //   db.createCollection('test', {w:1}, function(err, collection) {});
 
 // });
-
-
-
-
-
-
-
-router.get('/stuff', (request, response) => {
-    var urlParts = url.parse(request.url, true);
-    var parameters = urlParts.query;
-    var myParam = parameters.myParam;
-    // e.g. myVenues = 12;
-
-    var myResponse = `I multiplied the number you gave me (${myParam}) by 5 and got: ${myParam * 5}`;
-
-    response.json({ message: myResponse });
-});
-
-
-// this array is used for identification of allowed origins in CORS
-const originWhitelist = ['http://localhost:3000', 'https://heroku-test-node-api.herokuapp.com/'];
-
-// middleware route that all requests pass through
-router.use((request, response, next) => {
-    console.log('Server info: Request received');
-
-    let origin = request.headers.origin;
-
-    // only allow requests from origins that we trust
-    if (originWhitelist.indexOf(origin) > -1) {
-        response.setHeader('Access-Control-Allow-Origin', origin);
-    }
-
-    // only allow get requests, separate methods by comma e.g. 'GET, POST'
-    response.setHeader('Access-Control-Allow-Methods', 'GET');
-    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    response.setHeader('Access-Control-Allow-Credentials', true);
-
-    // push through to the proper route
-    next();
-});
-
-
-app.listen(process.env.PORT || port, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
-// set the server to listen on port 3000
-// app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`) );
