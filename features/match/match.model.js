5eca05e1b0f2923072781c342a7f16569364565d
Bruno Barbosa <bruno.barbosa@keyrus.com.br>
2018-11-26 19:29:23 -0200

    showing timeline

--- features/match/match.model.js
+++ features/match/match.model.js
@@ -9,13 +9,16 @@ const schema = new Schema({
     location: { type: String, required: true },
     homeTeam: { type: Object, required: true },
     awayTeam: { type: Object, required: true },
-    matchStats: { type: Object, required: false },
+    matchManager: { type: Object, required: false },
+    lineup: { type: Object, required: false },
+    timeline: { type: Object, required: false },
+    stats: { type: Object, required: false },
     homeTeamConfirmation: { type: Boolean, required: false },
     awayTeamConfirmation: { type: Boolean, required: false },
     status: { type: String, required: false, default:"CREATED" },
     createdDate: { type: Date, default: Date.now }
 });
-
+ 
 schema.set('toJSON', { virtuals: true });
 
 module.exports = mongoose.model('Match', schema);
